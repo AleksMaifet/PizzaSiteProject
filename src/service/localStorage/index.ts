3d3c2181081b027dc87424feebafa95df21cd039
AleksMaifet <aleksmaifet@gmail.com>
2022-01-16 12:55:53 +0300

    connected localStorage to project by persistedState in store

---
+++ src/service/localStorage/index.ts
@@ -0,0 +1,18 @@
+import { AppStoreType } from 'store/store';
+
+export const loadState = (): undefined => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+export const saveState = (state: AppStoreType): void => {
+  const serializedState = JSON.stringify(state);
+  localStorage.setItem('state', serializedState);
+};
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,6 @@
 import { combineReducers, createStore } from 'redux';
 
+import { loadState, saveState } from 'service/localStorage';
 import { appReducer } from 'store/reducers/appReducer';
 import { cartPizzasReducer } from 'store/reducers/cartPizzasReducer';
 import { pizzaReducer } from 'store/reducers/pizzaReducer';
@@ -10,9 +11,15 @@ const reducers = combineReducers({
   cartPizza: cartPizzasReducer,
 });
 
-export const store = createStore(reducers);
+const persistedState = loadState();
+
+export const store = createStore(reducers, persistedState);
 
 export type AppStoreType = ReturnType<typeof reducers>;
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 // @ts-ignore
 
